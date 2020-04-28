42aa4e6b31871bf61e421cb4466fbc83faaa13a0
ec-han <onyxonxy@gmail.com>
2020-04-28 20:56:12 +0900

    index.js update

--- back/index.js
+++ back/index.js
@@ -1,15 +1,37 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const config = require('./config/db-config.json');
+const config = require('./config/key')
+const { User } = require("./models/User")
 
-mongoose.connect(config.addr, {
+// application/x-www-form-urlencoded 이렇게 된 데이터를 분석해서 가져올 수 있게 해 줌
+app.use(bodyParser.urlencoded({ extended: true }))
+// application/json 타입으로 된 것을 분석해서 가져올 수 있게 해 줌
+app.use(bodyParser.json())
+
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => res.send('Hello World! 안녕하세요!'))
+app.get('/', (req, res) => res.send('Hello World! 안녕!'))
+
+app.post('/register', (req, res) => {
+    // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
