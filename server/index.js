ef81e729295db1d34bd7f70d01a7094c3e14da78
ec-han <onyxonxy@gmail.com>
2020-05-01 20:39:47 +0900

    경로 설정, 미들웨어 설치, concurrently로 dev 명령어 추가

--- server/index.js
+++ server/index.js
@@ -19,6 +19,9 @@ mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
+app.get('/api/hello', (req, res) => {
+    res.send('Hello World~~!! ')
+})
 
 app.get('/', (req, res) => res.send('Hello World! 안녕!'))
 
